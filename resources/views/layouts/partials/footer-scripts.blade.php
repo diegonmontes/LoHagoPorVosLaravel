767547ee1fa868b70507ddfa3d186a228e0ffd8b
ale <alejandrowasilewski@gmail.com>
2019-10-14 20:45:11 -0300

    tiempoDeEsperaAspirantes
    arreglo vista anuncios

--- resources/views/layouts/partials/footer-scripts.blade.php
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -1,3 +1,17 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.6.3/jquery-ui-timepicker-addon.min.js"></script>
+
+    <script>
+
+            $('#datepicker').datetimepicker({
+                altField: "#datepickerAlt",
+                altFieldTimeOnly: false,
+                altFormat: "yy-mm-dd",
+                controlType: 'select',
+                oneLine: true,
+                altTimeFormat: "H:m",
+            });
+      </script>
