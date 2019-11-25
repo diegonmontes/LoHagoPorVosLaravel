ff13677664b21b50f43ab4c1a3a885a04c5576c9
ale <alejandrowasilewski@gmail.com>
2019-11-25 15:53:55 -0300

    arreglado css

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -2,6 +2,8 @@
 <title>Lo hago por vos</title>
 
 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
 <!-- Icono -->
 <link rel="shortcut icon" href="images/lohagoporvos.ico">
 <!-- Fonts -->
@@ -10,18 +12,18 @@
 <!-- Styles -->
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 <link rel="stylesheet" type="text/css"  href="{{ asset('css/fontawesome-free/css/all.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+
 <link rel="stylesheet" type="text/css" href="{{ asset('css/estiloPropio.css') }}">
-<script src="http://localhost/LoHagoPorVosLaravel/public/js/app.js" defer></script>
-  <!-- Scripts -->
+{{-- <script src="{{asset('js/app.js')}}" defer></script> --}}
 
-<meta name="csrf-token" content="{{ csrf_token() }}">
-
+<!-- Scripts -->
 <script defer>
-    window.Laravel = {!! json_encode([
-        'csrfToken' => csrf_token(),
-        'pusherKey' => config('broadcasting.connections.pusher.key'),
-        'pusherCluster' => config('broadcasting.connections.pusher.options.cluster')
-    ]) !!};
+  window.Laravel = {!! json_encode([
+      'csrfToken' => csrf_token(),
+      'pusherKey' => config('broadcasting.connections.pusher.key'),
+      'pusherCluster' => config('broadcasting.connections.pusher.options.cluster')
+  ]) !!};
 </script>
 
 
